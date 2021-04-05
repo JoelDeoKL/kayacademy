dd125e11462d2a85ea7abd3427f65d1bc23cdccb
JoelDeoKL <joeldeocm88@gmail.com>
2021-04-05 23:09:03 +0200

    Ajout d'un lien pour edition des chaitre dans les chapitres meme

--- resources/views/adm/pages/examples/Chapitre/chapitre.blade.php
+++ resources/views/adm/pages/examples/Chapitre/chapitre.blade.php
@@ -236,7 +236,7 @@
                     </i>
                     View
                   </a>
-                  <a class="btn btn-primary btn-sm" href=""?>
+                  <a class="btn btn-primary btn-sm" href="{{route('module.edit', $module->id)}}"?>
                     <i class="fas fa-pencil-alt">
                     </i>
                     Edit
