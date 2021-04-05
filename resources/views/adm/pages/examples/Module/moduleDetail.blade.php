f6c5a824072e307481cc60ca2fc9dfd261fb92e5
JoelDeoKL <joeldeocm88@gmail.com>
2021-04-05 23:08:28 +0200

    Ajout d'un lien pour edition des chaitre dans le detail des modules

--- resources/views/adm/pages/examples/Module/moduleDetail.blade.php
+++ resources/views/adm/pages/examples/Module/moduleDetail.blade.php
@@ -236,7 +236,7 @@
                     </i>
                     View
                   </a>
-                  <a class="btn btn-primary btn-sm" href=""?>
+                  <a class="btn btn-primary btn-sm" href="{{route('chapitre.edit', $chapitre->id)}}"?>
                     <i class="fas fa-pencil-alt">
                     </i>
                     Edit
